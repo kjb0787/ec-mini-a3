fce2c6e31390ae3f5c60e10358a5fc89179432bc
marshWhale <39956222+kjb0787@users.noreply.github.com>
2021-10-27 20:15:17 -0700

    initial commit

---
+++ src/Canvas.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Canvas.css';
+
+export default function Canvas() {
+    return (
+        <div class="grid-area">
+            <div class="header">
+                <h1>Yu Cao</h1>
+            </div>
+            <div class="footer"></div>
+            <div class="leftbar"></div>
+            <div class="rightbar"></div>
+            <div class="content">
+                <div class="box"></div>
+            </div>
+        </div>
+    );
+}
